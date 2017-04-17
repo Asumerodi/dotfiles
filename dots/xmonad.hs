6848b296f5db452ef515c341382954804eb4e6c0
Timothy DeHerrera <tim.de9@gmail.com>
2017-04-17 14:48:39 -0600

    use maim and farbfeld for screenshots instead of scrot

--- dots/xmonad.hs
+++ dots/xmonad.hs
@@ -56,11 +56,31 @@ main = xmonad $ ewmh $ desktopConfig
       , ( ( modm .|. shiftMask, xK_l                    )
         , spawn "loginctl lock-session"                                 )
         -- screenshot
-      , ( ( modm , xK_Print                             )
-        , spawn "scrot $(date '+%s')_'$wx$h'.png -e 'mv $f ~/shots'"    )
+      , ( ( modm , xK_Print                              )
+        , spawn "maim -u \
+            \ | png2ff | lrzip -zp1 -L9 \
+            \ > ~/shots/$(date +%m.%d.%y_%I.%M.%S_%p).ff.lrz" )
         -- screenshot focused window
-      , ( ( modm .|. shiftMask, xK_Print                )
-        , spawn "scrot -u $(date '+%s')_'$wx$h'.png -e 'mv $f ~/shots'" )
+      , ( ( modm .|. shiftMask, xK_Print                 )
+        , spawn "maim -u -i$(xdotool getactivewindow) \
+            \ | png2ff \
+            \ | lrzip -zp1 -L9 \
+            \ > ~/shots/$(date +%m.%d.%y_%I.%M.%S_%p).ff.lrz" )
+        -- screenshot selection to clipboard
+      , ( ( modm .|. controlMask, xK_Print               )
+        , spawn "maim -s -u \
+            \ | xclip -selection clipboard -t image/png"      )
+        -- screenshot selection file
+      , ( ( modm .|. controlMask .|. shiftMask, xK_Print )
+        , spawn "maim -s -u \
+            \ | png2ff \
+            \ | lrzip -zp1 -L9 \
+            \ > ~/shots/$(date +%m.%d.%y_%I.%M.%S_%p).ff.lrz" )
+        -- screenshot selection to imgur and paste url in clipboard
+      , ( ( modm .|. shiftMask, xK_i                     )
+        , spawn "maim -s -u /tmp/img.png; \
+            \ imgurbash.sh /tmp/img.png; \
+            \ rm /tmp/img.png"                                )
         -- toggle touchpad
       , ( ( 0                 , xF86XK_TouchpadToggle   )
         , spawn "touchtoggle"                                           )
