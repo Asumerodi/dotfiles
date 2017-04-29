4cbc6f227b35e082ffd25a441cf954454aec14b4
Timothy DeHerrera <tim.de9@gmail.com>
2017-04-29 14:45:17 -0600

    set default font for dzvol

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -20,13 +20,15 @@ myLayout = smartBorders
       tiled    = Tall nmaster delta ratio
       nmaster  = 1
       ratio    = 309/500
-      delta    = 9/500
+      delta    = 1/1000
 
 modm = mod4Mask
 
 fullToggle = do
   spawn "stoggle"
   sendMessage $ Toggle FULL
+
+dzcall = "dzvol -fn 'DejaVu Sans Mono for Powerline'"
 
 main = xmonad $ ewmh $ desktopConfig
   { terminal   = "st"
@@ -39,16 +41,16 @@ main = xmonad $ ewmh $ desktopConfig
   } `additionalKeys`
         -- toggle fullscreen, dpms and xautolock
       [ ( ( modm              , xK_f                     )
-        , fullToggle                                        )
+        , fullToggle                                          )
         -- lower volume
       , ( ( 0                 , xF86XK_AudioLowerVolume  )
-        , spawn "pactl set-sink-volume 0 -1%; dzvol"          )
+        , spawn $ "pactl set-sink-volume 0 -1%; " ++ dzcall   )
         -- raise volume
       , ( ( 0                 , xF86XK_AudioRaiseVolume  )
-        , spawn "pactl set-sink-volume 0 +1%; dzvol"          )
+        , spawn $ "pactl set-sink-volume 0 +1%; " ++ dzcall   )
         -- mute volume
       , ( ( 0                 , xF86XK_AudioMute         )
-        , spawn "pactl set-sink-mute 0 toggle; dzvol"         )
+        , spawn $ "pactl set-sink-mute 0 toggle; " ++ dzcall  )
         -- start qutebrowser
       , ( ( modm              , xK_b                     )
         , spawn "qutebrowser --backend webengine"             )
